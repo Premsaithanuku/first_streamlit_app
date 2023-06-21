4ead4fd0743645499783835cb045934d32cb9a2f
Premsaithanuku <137286475+Premsaithanuku@users.noreply.github.com>
2023-06-21 17:05:30 +0530

    Update streamlit_app.py
    added title

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,6 +2,3 @@ import streamlit
 
 streamlit.title('My Parents New Healthy Diner')
 streamlit.title('Breakfast')
-streamlit.title('Omega 3 and Blueberry Oatmeal')
-streamlit.title('Kale, Spinach and Rocket Smoothie')
-streamlit.title('Hard Boiled Free-Range Egg')
