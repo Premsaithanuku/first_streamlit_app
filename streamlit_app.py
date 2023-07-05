0f65a61beeb11a9eefa4efaaa7780634cb9f4df1
Premsaithanuku <137286475+Premsaithanuku@users.noreply.github.com>
2023-07-05 11:21:39 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -24,7 +24,7 @@ fruits_to_show = my_fruit_list.loc[fruits_selected]
 
 # Display the table on the page.
 streamlit.dataframe(fruits_to_show)
-
+#h
 #New section to display fruityvice api response
 streamlit.header("Fruityvice Fruit Advice!")
 try:
